45ca2277aa1de625f1e1c03249caf91c75c6413c
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-11 13:01:45 +0300

    added testSpring and first bean to pom.xml

---
+++ src/main/java/org/zhuravlev/springcourse/TestSpring.java
@@ -0,0 +1,10 @@
+package org.zhuravlev.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring  {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    }
+}
