fc4ea231491e4b62dcf91df1027050aa96ac5862
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-11 13:09:07 +0300

    added code to main in TestSpring
    added context and var to test how getBean works

--- src/main/java/org/zhuravlev/springcourse/TestSpring.java
+++ src/main/java/org/zhuravlev/springcourse/TestSpring.java
@@ -2,9 +2,13 @@ package org.zhuravlev.springcourse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.print.attribute.standard.JobKOctets;
+
 public class TestSpring  {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
     }
 }
