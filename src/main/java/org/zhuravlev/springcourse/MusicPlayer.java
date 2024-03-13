ea929357b500f70a78255675e90a0c6705dc7952
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-13 13:58:50 +0300

    Added interface Music and class MusicPlayer
    Added two classes that is implementation of interface Music. Added class MusicPlayer for showing the example of IOT(inversion of control)

---
+++ src/main/java/org/zhuravlev/springcourse/MusicPlayer.java
@@ -0,0 +1,15 @@
+package org.zhuravlev.springcourse;
+
+public class MusicPlayer {
+    private Music music;
+
+    public MusicPlayer(Music music)
+    {
+        this.music = music;
+    }
+
+    public void playMusic()
+    {
+        System.out.println("Playing: " + this.music);
+    }
+}
