1d80959f9245cb30992a012ea6d9fad97fc6252a
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-13 14:42:38 +0300

    Modify musicPlayer
    Added two private fields to musicPlayer and setters and getters for them. Also modify pom.xml to try DI.

--- src/main/java/org/zhuravlev/springcourse/MusicPlayer.java
+++ src/main/java/org/zhuravlev/springcourse/MusicPlayer.java
@@ -2,6 +2,24 @@ package org.zhuravlev.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
 
     //IOC(inversion of control)
     public MusicPlayer(Music music)
