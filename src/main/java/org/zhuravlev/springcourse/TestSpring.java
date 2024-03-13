06ff8810d996e5f7e462a8d701e9548d43fe7dc3
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-13 14:17:18 +0300

    Test DI(dependency injection)
    Add bean MusicPlayer and with DI added to <constructor-arg> reference to musicBean

--- src/main/java/org/zhuravlev/springcourse/TestSpring.java
+++ src/main/java/org/zhuravlev/springcourse/TestSpring.java
@@ -7,8 +7,7 @@ import javax.print.attribute.standard.JobKOctets;
 public class TestSpring  {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
     }
