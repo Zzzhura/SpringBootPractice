c8f49cfad02bdb6440c666ff076a1b2bb1c2f973
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-13 14:24:46 +0300

    Added contructor in MusicPlayer and tring to add DI with MusicPlayer setter

--- src/main/java/org/zhuravlev/springcourse/MusicPlayer.java
+++ src/main/java/org/zhuravlev/springcourse/MusicPlayer.java
@@ -3,10 +3,18 @@ package org.zhuravlev.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    //IOC(inversion of control)
     public MusicPlayer(Music music)
     {
         this.music = music;
     }
+
+    public MusicPlayer(){}
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
 
     public void playMusic()
     {
