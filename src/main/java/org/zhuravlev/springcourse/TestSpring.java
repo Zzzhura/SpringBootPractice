e6b984c4617dbf44e65c2307d9812c259d12f01b
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-15 14:49:41 +0300

    FACTORY-METHOD test
    So, main idea of factory-methods is so to extand interface easier
    Also if we have singleton scope factory-method will be called once!!!!!!!
    Need to read: https://refactoring.guru/ru/design-patterns/factory-method

--- src/main/java/org/zhuravlev/springcourse/TestSpring.java
+++ src/main/java/org/zhuravlev/springcourse/TestSpring.java
@@ -7,7 +7,6 @@ public class TestSpring  {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
         System.out.println(classicalMusic.getSong());
         context.close();
     }
