bf049417009069c9f7724278723fb2f999acdc21
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-15 14:36:58 +0300

    PROTOTYPE SCOPE EXAMPLE(IMPORTANT!!!!!)
    If we use scope - "prototype" Spring gives full control of bean lifecycle to client. But if we use singleton it controls lifecycle.
    REMEMBER: If scope prototype we have to manage the bean lifecycle, main problem that we can have memory leaks.

--- src/main/java/org/zhuravlev/springcourse/TestSpring.java
+++ src/main/java/org/zhuravlev/springcourse/TestSpring.java
@@ -7,6 +7,7 @@ public class TestSpring  {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
         System.out.println(classicalMusic.getSong());
         context.close();
     }
