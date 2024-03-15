38c8a7e7354a0ddf6d0ce36997745fbf69674851
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-15 14:19:12 +0300

    Testing bean scopes
    If we have stateless object best options to use is "singleton". This type of scope create only one object, and if we call from context.getBean() it will refer us to one object.
    To make that clear if we create two object by getting bean and change something in first object, this  changes will also be applied to second object. If we have stateful object we have to use scope- "prototype"

--- src/main/java/org/zhuravlev/springcourse/TestSpring.java
+++ src/main/java/org/zhuravlev/springcourse/TestSpring.java
@@ -2,15 +2,17 @@ package org.zhuravlev.springcourse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import javax.print.attribute.standard.JobKOctets;
 
 public class TestSpring  {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        boolean comparison = (musicPlayer2 == musicPlayer1);
+        System.out.println(comparison);
+        musicPlayer1.playMusic();
+        System.out.println(musicPlayer1.getName());
+        System.out.println(musicPlayer1.getVolume());
         context.close();
     }
 }
