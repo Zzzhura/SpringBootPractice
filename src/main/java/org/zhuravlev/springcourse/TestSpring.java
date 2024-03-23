511142b48562ebfd3d2a31eb15ba23bffa70b6ed
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-23 15:39:33 +0300

    added annotation @Autowired to show how we can do DI without xml file

--- src/main/java/org/zhuravlev/springcourse/TestSpring.java
+++ src/main/java/org/zhuravlev/springcourse/TestSpring.java
@@ -7,11 +7,8 @@ public class TestSpring  {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Music music = context.getBean("musicBean", ClassicalMusic.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
-        Music rapMusic = context.getBean("rapMusic", Music.class);
-        MusicPlayer musicPlayer1 = new MusicPlayer(rapMusic);
-        musicPlayer1.playMusic();
         context.close();
     }
 }
