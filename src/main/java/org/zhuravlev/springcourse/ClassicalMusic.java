a2d58567f062f1512b3b469d5c9a16ec44c9ba59
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2024-03-23 15:52:03 +0300

    added to private fields in MusicPlayer and with @Autowired added DI in MusicPlayer constructor

--- src/main/java/org/zhuravlev/springcourse/ClassicalMusic.java
+++ src/main/java/org/zhuravlev/springcourse/ClassicalMusic.java
@@ -2,6 +2,7 @@ package org.zhuravlev.springcourse;
 
 import org.springframework.stereotype.Component;
 
+@Component
 public class ClassicalMusic implements Music{
     private ClassicalMusic() {}
 
--- src/main/java/org/zhuravlev/springcourse/MusicPlayer.java
+++ src/main/java/org/zhuravlev/springcourse/MusicPlayer.java
@@ -6,10 +6,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
     @Autowired
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    @Autowired
+    private RapMusic rapMusic;
+
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RapMusic rapMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rapMusic = rapMusic;
+    }
 
     public void playMusic()
     {
-        System.out.println("Playing: " + this.music.getSong());
+        System.out.println("Playing: " + this.classicalMusic.getSong());
+        System.out.println("Playing: " + this.rapMusic.getSong());
     }
 }
